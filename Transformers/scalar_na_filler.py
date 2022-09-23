d5b50d23ce5fff00afa54d9c7a10cd289660553c
Syed Muhammad Kamran Shafqat <100029901+SyedKamran0051@users.noreply.github.com>
2022-09-23 16:26:51 +0500

    pipeline/FEng/PreProcessing

--- Transformers/scalar_na_filler.py
+++ Transformers/scalar_na_filler.py
@@ -1,3 +1,5 @@
+from multiprocessing.sharedctypes import Value
+from re import S
 from pyspark.ml import Transformer
 from pyspark.sql import DataFrame
 from pyspark.ml.param.shared import HasInputCols
@@ -5,26 +7,19 @@ from pyspark.ml.param import Param, Params, TypeConverters
 from pyspark import keyword_only
 import pyspark.sql.functions as F
 
-class ScalarNAFiller(Transformer, HasInputCols):
+class ScallerNAFiller(Transformer):
     @keyword_only
-    def __init__(self, inputCols=None):
+    def __init__(self):
         super().__init__()
         self._setDefault()
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
-    def setParams(self, inputCols=None):
+    def setParams(self):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
     
-    def setInputCols(self, new_inputCols):
-        return self.setParams(inputCols=new_inputCols)
-
     def _transform(self, df:DataFrame):
-        input_columns = self.getInputCols()
-        for column in input_columns:
-            df = df.withColumn(column, df[column].fillna(6.0))
+        df = df.fillna(value=0)
         return df
-
-        
